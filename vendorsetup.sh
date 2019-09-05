313ddc10f8aa6b8312cdc85585196be997dcad70
Surge Raval <Surge1223@gmail.com>
2019-09-05 14:17:44 +0200

    disable these for now

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,17 +1,17 @@
-aosp_devices=('blueline' 'bonito' 'crosshatch' 'dragon' 'marlin' 'shamu' 'taimen' 'mata')
-caf_devices=('cheeseburger' 'dumpling' 'nash' 'oneplus3' 'potter' 'tenderloin')
+#aosp_devices=('blueline' 'bonito' 'crosshatch' 'dragon' 'marlin' 'shamu' 'taimen' 'mata')
+#caf_devices=('cheeseburger' 'dumpling' 'nash' 'oneplus3' 'potter' 'tenderloin')
 
 function lunch_devices() {
     add_lunch_combo du_${device}-user
     add_lunch_combo du_${device}-userdebug
 }
 
-if [[ $( grep -i "caf" manifest/README.md) ]]; then
-    for device in ${caf_devices[@]}; do
-        lunch_devices
-    done
-else
-    for device in ${aosp_devices[@]}; do
-        lunch_devices
-    done
-fi
+#if [[ $( grep -i "caf" manifest/README.md) ]]; then
+#    for device in ${caf_devices[@]}; do
+#        lunch_devices
+#    done
+#else
+#    for device in ${aosp_devices[@]}; do
+#        lunch_devices
+#    done
+#fi
